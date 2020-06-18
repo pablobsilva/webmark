c29e95d94d20a31232f535d6a725bf80226c458c
zigreth <zigreth@gmail.com>
2020-06-18 03:50:26 -0400

    todo en view eliminado, creado desde 0 vistas new.

--- views/solicitudes/gestionar.php
+++
@@ -1,7 +0,0 @@
-<?php require_once '/var/www/WebMarketTest/views/layouts/headerempresa.php'?>
-
-
-<h1> HELLO WORLD  </h1>
-<h1> F </h1>
-
-<?php require_once '/var/www/WebMarketTest/views/layouts/footerempresa.php'; ?>
