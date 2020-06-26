c512d03a28603f3e1e100135d61058eeb6b2d964
pablo <pablo.bsilva8@gmail.com>
2020-06-26 07:44:03 +0000

    ajustes

--- views/administrador/producto/productos.php
+++ views/administrador/producto/productos.php
@@ -32,7 +32,6 @@
 
             <!-- CONTENT PAGE -->
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-
                 <div
                     class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Productos</h1>
@@ -81,12 +80,9 @@
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-
                 </div>
-
             </main>
             <!-- CONTENT PAGE -->
-
             <!-- Modal -->
             <?php require '/var/www/WebMarketTest/views/administrador/producto/modales/agregar-producto.php' ?>
             <!-- Modal -->
