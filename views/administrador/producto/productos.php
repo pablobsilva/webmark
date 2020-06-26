5f0272239bd2ff010994be317639aa8998920f8a
zigreth <zigreth@gmail.com>
2020-06-26 04:01:30 -0400

    modificado boton ruso

--- views/administrador/producto/productos.php
+++ views/administrador/producto/productos.php
@@ -74,8 +74,11 @@
                                 <td><?php echo $producto->categoria;?></td>
                                 <td><?php echo $producto->empresa;?></td>
                                 <td><?php echo $producto->stock;?></td>
-                                <td> <span class='editProduct' data-product-id="<?php echo $producto->idProducto;?>"
-                                        data-feather="edit"> </span> </td>
+                                <td>    
+                                    <button class='btn btn-default btn-sm editProduct' data-product-id="<?php echo $producto->idProducto;?>">
+                                        <a data-feather="edit"></a>
+                                    </button>
+                                </td>
                                 <td> <span data-feather="trash-2"></span> </td>
                             </tr>
                             <?php endforeach; ?>
